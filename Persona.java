5a5f4a04be70e18f44cf4668c8b4f34873d792bf
luisRxo18 <32626310+luisRxo18@users.noreply.github.com>
2017-10-11 18:34:21 -0500

    Update Persona.java

--- Persona.java
+++ Persona.java
@@ -10,28 +10,33 @@ public class Persona {
 	public Persona(String nombre, int edad){
 		this.nombre=nombre;
 		this.edad=edad;
+        mascotas = new ArrayList<Mascota>();
 	}
 	public String getNombre(){
-		return nombre;
+		return this.nombre;
 	}
 	public void setNombre(String nombre){
 		this.nombre=nombre;
 	}
 	public int getEdad(){
-		return edad;
+		return this.edad;
 	}
 	public void setEdad (int edad){
 		this.edad=edad;
 
 	}
 	public void visitaRefugio(Refugio refugio){
-		enRefugio = refugio;
+		enRefugio=refugio;
 	}
 	public void adopta(){
-		mascotas.add(enRefugio.eligeMascota())
+        
+        mascotas.add(enRefugio.eligeMascota());
+            
+
 	}
 	public void muestraMascotas(){
-        	for(Mascota mas : mascotas){
-            		System.out.println(mas);
-       		 }
+        for(Mascota mas : mascotas){
+            System.out.println(mas);
+        }
+	}
 }
